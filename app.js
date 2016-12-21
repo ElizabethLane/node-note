7805c53f38e658736d683138d9fb2b299d704098
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-12-20 17:26:47 -0800

    integrated a template string and added os module

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 console.log('Starting App');
 
 const fs = require('fs');
-//This says fetch all requires of 'fs' variable and store
+//This says fetch all requires of 'fs' variable (string is  
+//keyword in node bundled modules) and store
 //in fs constant
 //**NOTE** const: block-scoped, immutable variables, results
 //in optimal performation as constant will be replaced 
 //at compile-time
 
-fs.appendFile('greetings.txt', 'Hello World', function(err) {
+const os = require('os');
+
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
+    //The dollar sign curly braces is 'template strings' in ES6
+    //The tick is in front of the 1 on the keyboard
+    //Inside the curly braces simply insert a variable
     if (err) {
         console.log('Unable to write file');
     }
@@ -15,3 +23,6 @@ fs.appendFile('greetings.txt', 'Hello World', function(err) {
 
 //We added the error callback function as node throws
 //error without it
+
+//fs.appendfile -if first arg file doesn't exist
+//it will be created
