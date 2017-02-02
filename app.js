e4eb26ae6dad2cd2ea0e8dcc90bb8740e03af483
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2017-02-02 15:55:09 -0800

    playing with lodash

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 console.log('Starting App.js');
 
 const fs = require('fs');
-//This says fetch all requires of 'fs' variable (string is  
+//This says fetch all requires of 'fs' variable (string is
 //keyword in node bundled modules) and store
 //in fs constant
 //**NOTE** const: block-scoped, immutable variables, results
-//in optimal performation as constant will be replaced 
+//in optimal performation as constant will be replaced
 //at compile-time
 
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
+//Will look at core modules first to find lodash
+//Will see it is not there, look at node_modules next
 
 // './' --> points to current directory
+var filteredArray = _.uniq(['and', 1, 'and', 2])
+console.log(filteredArray);
 
-
-
-var add = notes.add(4,3);
-console.log(add);
 
 // var user = os.userInfo();
 
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, 
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`,
 //     function(err) {
 //     //The dollar sign curly braces is 'template strings' in ES6
 //     //The tick is in front of the 1 on the keyboard
